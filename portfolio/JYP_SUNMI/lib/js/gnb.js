7dc6a224a77b19a446b0991790ec10555f0f7ec8
coilrainbow <coilrainbow@gmail.com>
2016-08-25 04:51:48 +0900

    gogo

--- portfolio/JYP_SUNMI/lib/js/gnb.js
+++ portfolio/JYP_SUNMI/lib/js/gnb.js
@@ -1,21 +1,36 @@
- $(function(){
+ $(function() {
 
-    $('.nav').click(function(){
-        // $('.nav > a').text('Close Menu')
-        $('.nav ul').toggle(0,function(){
-            $('.nav > a').text('Close Menu')    
-        })
-        /*$('.nav ul').slideDown(500,function(){
-            $('.nav > a').text('Close Menu')    
-        })*/
-        
-        return false
-    })
+     //start navFn
+     $(window).resize(function() {
+             if ($('body').width() > 767) {
+                 $('.nav >a:first').css({
+                     display: 'none'
+                 });
+                 $('.gnbList').css({
+                     display: 'block'
+                 })
+             } else if ($('body').width() < 767) {
+                 $('.nav >a:first').css({
+                     display: 'block'
+                 });
+                 $('.nav .gnbList').css({
+                     display: 'none'
+                 });
+                 $('.changeText').text('Main Menu')
+             }
 
-    
-    if($('body').width() >= 768){
-        $('.nav').off()
-    }
-    
-    
-})
+         })
+         //end navFn
+
+
+
+     // start nav click event
+     $('.nav > a').click(function() {
+         $('.nav ul').toggle();
+         $('.openMenu').text(function(i, v) {
+             return v === 'Main Menu' ? 'Close Menu' : 'Main Menu'
+                 // v가 MM이라는 조건이? true면 CM false면 MM
+         });
+     });
+     // start nav click event
+ })
